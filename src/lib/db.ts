cb31e038b6e5ac9984f82f4de01817e510e4dbe2
meew12 <meeworkout@gmail.com>
2026-08-27 01:00:11 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,14 +6,15 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const TURSO_URL = 'libsql://jose-demo-interf.aws-us-east-2.turso.io'
-const TURSO_TOKEN = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODc3OTgwMjQsImlkIjoiMDFhMDQxMTAtMzQwMS03Nzg1LTllMjYtMjM1MDcxMDg5MTk4Iiwia2lkIjoibmZ6RGk1ajJlekZGajVaN2RpczUxZmhvZXp1STZDcGVmcGtxQ0dVM096dyIsInJpZCI6ImVjZmNhNzE5LTcxZGMtNDczMS1hNGFiLTZkZDU4YTdjNTEzNyJ9.-n-PJTRxCHJxpPPWRv9td7b03Fy1wbw6Mzf-OFRqFFZI8OQdbsWjMWveDOwXDy3vOn36AhtnMQ4BfcyUoJICBA'
+// URL completa con token incluido (formato que Prisma espera)
+const DATABASE_URL_WITH_TOKEN = 'libsql://jose-demo-interf.aws-us-east-2.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODc3OTgwMjQsImlkIjoiMDFhMDQxMTAtMzQwMS03Nzg1LTllMjYtMjM1MDcxMDg5MTk4Iiwia2lkIjoibmZ6RGk1ajJlekZGajVaN2RpczUxZmhvZXp1STZDcGVmcGtxQ0dVM096dyIsInJpZCI6ImVjZmNhNzE5LTcxZGMtNDczMS1hNGFiLTZkZDU4YTdjNTEzNyJ9.-n-PJTRxCHJxpPPWRv9td7b03Fy1wbw6Mzf-OFRqFFZI8OQdbsWjMWveDOwXDy3vOn36AhtnMQ4BfcyUoJICBA'
 
 function createPrismaClient() {
+  // Crear cliente libsql directo con la URL completa
   const libsql = createClient({
-    url: TURSO_URL,
-    authToken: TURSO_TOKEN,
+    url: DATABASE_URL_WITH_TOKEN,
   })
+  
   const adapter = new PrismaLibSQL(libsql)
   return new PrismaClient({ adapter })
 }
