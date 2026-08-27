cdfe8bf3f9a878df4addd1ae747f9a4856aaa815
meew12 <meeworkout@gmail.com>
2026-08-27 02:27:09 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
-const globalForDb = globalThis as unknown as {
-  db: ReturnType<typeof createClient> | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-function createDb() {
-  return createClient({
+function createPrismaClient() {
+  const libsql = createClient({
     url: 'libsql://jose-demo-interf.aws-us-east-2.turso.io',
     authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODc3OTgwMjQsImlkIjoiMDFhMDQxMTAtMzQwMS03Nzg1LTllMjYtMjM1MDcxMDg5MTk4Iiwia2lkIjoibmZ6RGk1ajJlekZGajVaN2RpczUxZmhvZXp1STZDcGVmcGtxQ0dVM096dyIsInJpZCI6ImVjZmNhNzE5LTcxZGMtNDczMS1hNGFiLTZkZDU4YTdjNTEzNyJ9.-n-PJTRxCHJxpPPWRv9td7b03Fy1wbw6Mzf-OFRqFFZI8OQdbsWjMWveDOwXDy3vOn36AhtnMQ4BfcyUoJICBA',
   })
+  const adapter = new PrismaLibSQL(libsql)
+  return new PrismaClient({ adapter })
 }
 
-export const db = globalForDb.db ?? createDb()
+export const db =
+  globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForDb.db = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
